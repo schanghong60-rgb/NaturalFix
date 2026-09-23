6d0cfc51e1d2a0fe08796ee9577b1067431ddfec
schanghong60-rgb <schanghong60@gmail.com>
2026-09-23 15:16:06 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// NaturalFix runtime configuration.
+// Same-origin Node server: leave API_BASE_URL empty.
+// GitHub Pages: set this to your HTTPS backend, e.g. https://naturalfix-api.example.com
+window.NATURALFIX_CONFIG = Object.freeze({
+  API_BASE_URL: ''
+});
